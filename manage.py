8b06e3bfbeb3666d13870d7b7948a845fea0d2cf
rlemayd <ralemay@miuandes.cl>
2020-11-08 10:04:35 -0300

    now should

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "tb_tutorial.settings.develop"
+        "DJANGO_SETTINGS_MODULE", "telegram_bot.settings.develop"
     )
     try:
         from django.core.management import execute_from_command_line
