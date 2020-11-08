d0e58c689ea7320374ab492d845d323d6c2adc42
rlemayd <ralemay@miuandes.cl>
2020-11-08 11:31:28 -0300

    tst 5

--- telegram_bot/settings/production.py
+++ telegram_bot/settings/production.py
@@ -11,7 +11,7 @@ ALLOWED_HOSTS.append("proyecto-4-rich-kath.herokuapp.com")
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 mongodb_user = "rlemayd"
-mongodb_password = "proyecto4pds"
+mongodb_password = "test123"
 mongodb_host = "cluster0.o8ztz.mongodb.net/Cluster0?retryWrites=true&w=majority"
 MONGO_CLIENT = MongoClient(f"mongodb+srv://{mongodb_user}:{mongodb_password}@{mongodb_host}")
 
