db03e094a1e2b7a28c9d8f684833635609d9895a
rlemayd <ralemay@miuandes.cl>
2020-11-08 11:40:20 -0300

    tst 7

--- telegram_bot/settings/production.py
+++ telegram_bot/settings/production.py
@@ -12,7 +12,7 @@ ALLOWED_HOSTS.append("proyecto-4-rich-kath.herokuapp.com")
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 mongodb_user = "rlemayd"
 mongodb_password = "test123"
-mongodb_host = "cluster0.o8ztz.mongodb.net/test?retryWrites=true&w=majority"
+mongodb_host = "cluster0.o8ztz.mongodb.net/cluster0?retryWrites=true&w=majority"
 MONGO_CLIENT = MongoClient(f"mongodb+srv://{mongodb_user}:{mongodb_password}@{mongodb_host}")
 
 MONGO_DB = MONGO_CLIENT.telegram_bot
