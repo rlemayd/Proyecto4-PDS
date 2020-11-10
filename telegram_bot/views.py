91f286ce4982096c65a126cca7a7c520d339f3ba
Richard Le May <ralemay@miuandes.cl>
2020-11-10 17:37:13 -0300

    adding statistics last_message

--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -135,7 +135,7 @@ class TutorialBotView(View):
             else:
                 chat["messages"].update({text: chat["messages"][text] + 1})
                 telegram_bot_collection.save(chat)
-            chat["last_message"].update(text)
+            chat.update({"last_message": text})
             telegram_bot_collection.save(chat)
 
         else:
