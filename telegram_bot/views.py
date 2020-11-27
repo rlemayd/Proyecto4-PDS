b34fd20cf6a64a390e881fb6d97cf5852ab519b0
Richard Le May <ralemay@miuandes.cl>
2020-11-27 16:42:33 -0300

    q9 think i fixed

--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -158,7 +158,6 @@ class TutorialBotView(View):
                 telegram_bot_collection.save(chat)
 
             if str(dateObtained) not in chat["all_words"]:
-                chat["all_words"] = str(dateObtained)
                 for i in text.split():
                     print("TESTTTTTTTTTT",i)
                     print("AAAAAAAAAAAAA", chat)
